29fe349eeaaf631295f008bc70430ce2b63655f2
Y-Datsenko <yevhenii.datsenko@gmail.com>
2020-05-29 20:04:35 +0300

    added Kotlin Spring example project

---
+++ src/main/kotlin/com/datsenko/yevhenii/workouts/WorkoutsApplication.kt
@@ -0,0 +1,11 @@
+package com.datsenko.yevhenii.workouts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkoutsApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkoutsApplication>(*args)
+}
---
+++ src/test/kotlin/com/datsenko/yevhenii/workouts/WorkoutsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.datsenko.yevhenii.workouts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkoutsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
